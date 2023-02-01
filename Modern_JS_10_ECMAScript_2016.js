83b661d68c8c569fb1f8411dc04bafaa2c5c28ac
Sameer Kumar <sameer2001kr@gmail.com>
2023-02-01 11:27:39 +0530

    six commit

--- Modern_JS_10_ECMAScript_2016.js
+++ Modern_JS_10_ECMAScript_2016.js
@@ -24,14 +24,72 @@ The destructing assignment syntax is Javascript expression that makes it poissib
 
 // ⭐ OBJECT DESTRUCTURING
 
-const myBioData = {
-    myFName : 'DRAKE',
-    myLName : 'THAPA',
-    myAge : 26
-}
+// const myBioData = {
+//     myFName : 'DRAKE',
+//     myLName : 'THAPA',
+//     myAge : 26
+// }
 
 // let age = myBioData.age;
 // console.log(age);
 
-let {myFName, myLName, myAge} = myBioData;
-console.log(myAge);
+// let {myFName, myLName, myAge} = myBioData;
+// console.log(myAge);
+
+// ⭐ OBJECT PROPERTIES
+
+// We can now use Dynamic Properties
+
+
+// let myName = 'Drake';
+// let myAge = 26;
+// const myBio = {
+//     myName : myName,
+// }
+
+// No need to write key and value, if both are same
+
+// let myName = "Drake Raj";
+// let myAge = 26;
+// const myBio = {
+//     myName : myName,
+//      myAge : myAge
+// }
+// const myBio = {myName, myAge};
+// console.log(myBio);
+
+// ⭐ SPREAD OPERATOR :
+
+// const colors = ['red', 'green', 'blue', 'white'];
+// const myColors = ['red', 'green', 'blue', 'white', 'yellow', 'black'];
+
+// 2nd Time add one more color on top and tell we need to write it again on myColor array to
+
+// const myFavColors = [...colors, 'yellow', 'green', 'blue', 'white'];
+// console.log(myFavColors);
+
+// 🧑‍💻 ES7 FEATRUES :
+
+// 1: Array include 
+const colors = ['red', 'green', 'blue', 'white', 'pink'];
+const isPresent = colors.includes('purple');
+console.log(isPresent);
+
+// 2: **
+console.log(2**3);
+
+
+// 🧑‍💻 ES8 FEATRUES :
+
+// String padding
+// Object.values()
+// Object.entries()
+
+const message = "My name is Drake";
+console.log(message);
+console.log(message.padStart(5));
+console.log(message.padEnd(10));
+
+const person = {name : 'FRED', age : 87};
+console.log(Object.values(person));
+console.log(Object.entries(person));
